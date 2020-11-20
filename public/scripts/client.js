99c9a5da40e7179d624b2ff243b72cad03f3b445
dlinardi <dlinardi98@gmail.com>
2020-11-20 03:05:19 +0000

    implemented loadTweets function ajax get request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,31 +7,6 @@
 
 $(document).ready(() => {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
   const createTweetElement = (tweetObj) => {
     const userObject = tweetObj.user;
     const contentObject = tweetObj.content;
@@ -97,8 +72,18 @@ $(document).ready(() => {
 
   });
 
+  const loadTweets = () => {
+    $
+      .ajax(
+        '/tweets', 
+        { method: "GET" }
+      )
+      .then(tweets => {
+        renderTweets(tweets);
+      })
+      .catch(error => console.log(error));
+  };
 
-
-  renderTweets(data);
+  loadTweets();
   
 });
